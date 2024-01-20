1778b1dd8cb5c4783eaf73f05b3602eca12be135
YosefSameh <139604190+YosefSameh@users.noreply.github.com>
2024-01-20 17:24:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// step 1
+
+let sideMenu = document .getElementById("side-menu")
+let slider = document .getElementById("slider")
+let item = document .getElementById("item")
+let imgSlider = document .getElementById("img-slider")
+let prev = document.getElementById("prev")
+let next = document.getElementById("next")
+
+
+// step 2
+
+let arrSlider = ["/img/samsung-1.jpg","/img/samsung-2.jpg","/img/samsung-3.jpg","/img/samsung-4.jpg"]
+let index = 0
+
+let imgInSlider = (idx)=>{
+    index = idx
+    imgSlider.src = arrSlider[index]
+}
+next.onclick = ()=>{
+    index++
+    if (index >= arrSlider.length) {
+        index = 0
+    }
+    imgInSlider(index)
+}
+prev.onclick = ()=>{
+    index--
+    if (index < 0 ) {
+        index = arrSlider.length-1 
+    }
+    imgInSlider(index)
+}
+
+
+
+
+
+
+
+
+
+
+
